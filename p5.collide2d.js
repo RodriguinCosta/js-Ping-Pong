7cb7d5e224eabf1cff774c479e599550eb17b27a
Rodrigo <00001112671067sp@al.educacao.sp.gov.br>
2024-09-22 21:45:08 -0300

    Uditando p5.collide2d.js

--- p5.collide2d.js
+++ p5.collide2d.js
@@ -6,6 +6,9 @@ Version v0.7.3 | June 22, 2020
 CC BY-NC-SA 4.0
 */
 
+
+console.log("### p5.collide v0.7.3 ###")
+
 p5.prototype._collideDebug = false;
 
 p5.prototype.collideDebug = function(debugMode){
